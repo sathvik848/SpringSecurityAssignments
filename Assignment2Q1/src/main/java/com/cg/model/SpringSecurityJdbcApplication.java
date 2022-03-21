7d35bbc11aeafde9e94763a6c129b3a83bd90e6f
sathvik848 <99522543+sathvik848@users.noreply.github.com>
2022-03-21 18:06:55 +0530

    Add files via upload

---
+++ Assignment2Q1/src/main/java/com/cg/model/SpringSecurityJdbcApplication.java
@@ -0,0 +1,11 @@
+package com.cg.model;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class SpringSecurityJdbcApplication {
+public static void main(String[] args) {
+SpringApplication.run(SpringSecurityJdbcApplication.class,
+args);
+}
+}
---
+++ Assignment2Q1/src/test/java/com/cg/model/Assignment2Q1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cg.model;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Assignment2Q1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
